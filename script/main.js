5c26c15159843832298fbed5ee61d79d505c31c1
sadachpardede <47018876+sadachpardede@users.noreply.github.com>
2023-06-28 19:42:30 +0700

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -68,7 +68,7 @@ const animationTimeline = () => {
             opacity: 0,
             y: 10
         },
-    "+=4")
+    "+=2")
     .to(".two",
         0.7,
         {
@@ -86,7 +86,7 @@ const animationTimeline = () => {
             opacity: 0,
             y: 10
         },
-    "+=2.3")
+    "+=2")
     .from(".four", 0.7, {
         scale: 0.2,
         opacity: 0,
