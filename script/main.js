11fb6af0072312aec1ae4d4ed5ee55d839b511da
sadachpardede <47018876+sadachpardede@users.noreply.github.com>
2023-06-28 19:15:22 +0700

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -86,7 +86,7 @@ const animationTimeline = () => {
             opacity: 0,
             y: 10
         },
-    "+=3")
+    "+=2")
     .from(".four", 0.7, {
         scale: 0.2,
         opacity: 0,
